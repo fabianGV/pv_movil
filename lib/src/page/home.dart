6d8d610df9c21b163345fed5b0319dcea5705c45
iver gonzalez vidal <iver.fabi13@gmail.com>
2021-03-04 23:03:35 -0600

    add menu lateral

---
+++ lib/src/page/home.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Punto de venta'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text('¡Bueno días!'),
+              accountEmail: Text('Fecha: yyyy-MM-dd'),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Text(
+                  'PV',
+                  style: TextStyle(fontSize: 40.0),
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Indicadores'),
+              leading: Icon(Icons.bar_chart_rounded),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Productos'),
+              leading: Icon(Icons.article),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
